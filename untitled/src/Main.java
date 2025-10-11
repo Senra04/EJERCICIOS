5aff93e12f25481c50cd4a5c3897699c5d94d173
Senra04 <senra04@gmail.com>
2025-10-11 12:18:48 +0200

    Ejercicio OK

---
+++ untitled/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre ="Diego Senra Gomez";
+        String calle = "C/ Manresa";
+        int numero = 51;
+        int piso = 3;
+        char letra = 'A';
+        String codigoPostal = "28034";
+        String ciudad ="Madrid";
+        String pais ="España";
+
+
+        System.out.println(nombre);
+        System.out.println(calle + "nº" + numero + "," +piso + letra);
+        System.out.println(codigoPostal + " " + ciudad);
+        System.out.println(pais);
+
+
+    }
+}
