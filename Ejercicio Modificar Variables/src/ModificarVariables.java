cb068fa30be66df0a6732b9870ee88f715b9a36c
Senra04 <senra04@gmail.com>
2025-10-17 17:01:23 +0200

    ok
     pc

---
+++ Ejercicio Modificar Variables/src/ModificarVariables.java
@@ -0,0 +1,16 @@
+public class ModificarVariables {
+    public static void main(String[] args) {
+        int puntuacion = 0; // Valor inicial
+        System.out.println("Puntuacion inicial "+ puntuacion);
+
+        //Primera modificacion
+        puntuacion = +5;
+        System.out.println("Despues de la primera modificacion: "+puntuacion);
+
+        puntuacion = 10;
+        System.out.println("Despues de la segunda modificacion: "+puntuacion);
+
+        puntuacion = 15;
+        System.out.println("Final: "+puntuacion);
+    }
+}
---
+++ Ejercicio. VariablesNombresDescriptivos/src/InformcacionLibro.java
@@ -0,0 +1,18 @@
+
+
+public class InformcacionLibro {
+    public static void main(String[] args) {
+        String titulo = "Don Quijote de la Mancha";
+        String autor = "Miguel de Cervantes";
+        int anioPublicacion = 1605;
+        int numeroPaginas = 863;
+        boolean disponible = true;
+
+        System.out.println("Titulo: "+titulo);
+        System.out.println("Autor: "+autor);
+        System.out.println("Año de publicacion: "+anioPublicacion);
+        System.out.println("¿Está disponible en la biblioteca?: "+disponible);
+
+    }
+}
+
